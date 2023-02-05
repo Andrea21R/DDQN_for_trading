1b821a17cc39b472ce06e3aa44dd013aacbd5984
Andrea Chiaverini <86592351+AndreaChiaverini-Etna@users.noreply.github.com>
2023-02-05 14:27:17 +0100

    data area created

--- data/resampler.py
+++ data/resampler.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from typing import *
 
 
 class Sampler:
@@ -12,29 +13,35 @@ class Sampler:
         return bars.iloc[0]
 
     @classmethod
-    def aggregate_bar(cls, bars: pd.DataFrame) -> pd.Series:
-        return bars.aggregate(
-            {
-                'open': cls._get_open,
-                'high': max,
-                'low': min,
-                'close': cls._get_close,
-            }
-        )
+    def aggregate_bar(cls, bars: pd.DataFrame) -> Union[pd.Series, None]:
+
+        if not bars.empty:
+            return bars.aggregate(
+                {
+                    'open': cls._get_open,
+                    'high': max,
+                    'low': min,
+                    'close': cls._get_close,
+                }
+            )
+        else:
+            return None
 
     @classmethod
     def resample(cls, bars: pd.DataFrame, freq: str = "1H") -> pd.DataFrame:
-        return bars.resample(rule=freq).apply(cls.aggregate_bar)
+        return bars.resample(rule=freq).apply(cls.aggregate_bar).dropna()
 
 
 if __name__ == "__main__":
 
     import os
 
-    file_name = "EURUSD_2022_1m.parquet"
-    new_freq = "1H"
+    dir_path      = os.getcwd() + rf"\dataset"
+    file_name     = "\EURUSD2022_1m.parquet"
+    new_file_name = "\EURUSD2022_1h.parquet"
+    new_freq      = "1H"
 
-    data = pd.read_parquet(os.getcwd() + rf"\dataset\{file_name}")
+    data = pd.read_parquet(dir_path + file_name)
     data_resampled = Sampler.resample(bars=data, freq=new_freq)
 
-
+    data_resampled.to_parquet(dir_path + new_file_name)
